90a64efd22b1ba538a1c89afd58627e098adbd6a
birdaNFT <birdaNFT@gmail.com>
2022-08-27 22:27:05 +0100

    Add get-assessed page

--- components/heading-large.tsx
+++ components/heading-large.tsx
@@ -9,7 +9,7 @@ interface HeadingLargeProps {
 
 export default function HeadingLarge(props: HeadingLargeProps) {
   return (
-    <div className="text-left">
+    <div>
       <h2 className={`${props.subColor} tracking-widest pb-4 uppercase`}>{props.subtitle}</h2>
       <h1 className="text-4xl xs:text-6xl sm:text-7xl tracking-normal xs:leading-tight sm:leading-tight font-extrabold text-[#E6E6E6]">
         <span className="block xl:inline">{props.span1}</span>{" "}
